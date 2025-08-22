1952ab7cdbd88c0c8346e3c506a82f8ecf3dc53a
Grigoriy Suhetskiy <LEXUSGS300@webizi.ru>
2025-08-22 10:53:47 +0300

    deleted:    bot/

--- bot/config.py
+++
@@ -1,4 +0,0 @@
-import os
-PUBLIC_BASE = os.getenv("PUBLIC_BASE_URL", "http://lavrpro.ru")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8362762788:AAG2IVIFES7nMplxOlJTynyS9s0qPmL9MqQ")
-API_BASE_URL = os.getenv("API_BASE_URL", "http://localhost:8000")
